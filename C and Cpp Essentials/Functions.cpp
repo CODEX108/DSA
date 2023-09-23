bf957766da1c99d74e184fdb93a424cf199d3808
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-09-23 19:23:11 +0530

    call by address

--- C and Cpp Essentials/Functions.cpp
+++ C and Cpp Essentials/Functions.cpp
@@ -2,7 +2,20 @@
 #include <iostream>
 using namespace std;
 
+//Call by address
+int swap(int *x,int *y){
+    int temp;
+    temp = *x;
+    *x=*y;
+    *y= temp;
+}
+
 int main()
 {
+    int num1=10;int num2=20;
+    swap(&num1,&num2);
+    cout<<"First Number: "<<num1<<endl;
+    cout<<"Second Nuber: "<<num2<<endl;
+    
    return 0;
 }
