ed87fb78c6a0f86b4739720054394e5adbc64655
Manjiri Milind Chavande <manjiri.c@fyle.in>
2023-09-24 12:43:24 +0530

    array inside struct

--- C and Cpp Essentials/Pointer_to_struct.cpp
+++ C and Cpp Essentials/Pointer_to_struct.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
+#define n 3
+
 using namespace std;
 
 
@@ -8,19 +10,69 @@ struct Rectangle
 {
     int length;
     int breadth;
+    int arr[5];
 };
 
 //this(call by ref)  works same as call by value but different object is not created in the heap
-int area(struct Rectangle &r1){
-    r1.length = 50;
-    return (r1.length*r1.breadth);
+// int area(struct Rectangle &r1){
+//     r1.length = 50;
+//     cout<<r1.breadth<<" "<<r1.length<<endl;
+//     return (r1.length*r1.breadth);
+// }
+
+//call by address
+int changeLength(struct Rectangle *p,int l){
+    p->length = l;
 }
+
+//returning a pointer of type structure
+//here fnction 'func' has created the strct inside the heap 
+struct Rectangle *func(){
+    struct Rectangle *p;
+    p=(struct Rectangle *)malloc(sizeof( struct Rectangle)); //dynamic memory allocation for rectangle
+    p->length=7;
+    p->breadth=15;
+    p[0].arr[0] = 20;
+    return p;
+}
+
+struct body
+{
+    double p[3]; // Position
+    double v[3]; // Velocity
+    double a[3]; // Acceleration
+    double radius;
+    double mass;
+};
 
 
 int main()
 {
-    struct Rectangle r1 = {10, 4};
-    cout << "Area of rectangle is : "<<area(r1) ;
+
+
+    struct Rectangle *ptr = func();
+    cout<<"Length "<<ptr->length<<" Breadth "<<ptr->breadth<<" Array element 1: "<<ptr[0].arr[0];
+    
+    
+    struct body bodies[n];
+    int a, b;
+    for(a = 0; a < n; a++)
+    {
+        for(b = 0; b < 3; b++)
+        {
+            bodies[a].p[b] = 0;
+            bodies[a].v[b] = 0;
+            bodies[a].a[b] = 0;
+        }
+        bodies[a].mass = 0;
+        bodies[a].radius = 1.0;
+    }
+
+    // struct Rectangle r1 = {10, 4};
+    // cout << "Area of rectangle is : "<<area(r1)<<" "<<r1.breadth<<" "<<r1.length<<endl;
+    // cout<<r1.breadth<<" "<<r1.length;
+
+    // changeLength(&r1,20);
     // struct Rectangle *p = &r;
     /*
     Access the member from the structure using pointer: *p.length =20;
